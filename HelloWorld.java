220bb8d6d6e6a2bc0f1a80c07fc86e28fa962ba4
Gustavo Peiretti <gustavo.peiretti@wnco.com>
2019-09-20 09:31:52 -0500

    Docker hello world Java example

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Running HelloWorld into docker");
+        System.out.println("Hello World Docker!!!");
+        System.out.println("Bye bye!");
+    }
+}
